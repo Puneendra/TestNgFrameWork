3741a146cf9aca249224757a80e4a9fb19c4094a
Puneendra <samanapuneendra@gmail.com>
2025-10-31 06:44:42 +0530

    Fixed merge conflicts and cleaned HomePage, RegisterPage

--- src/test/java/PageObjectClasess/HomePage.java
+++ src/test/java/PageObjectClasess/HomePage.java
@@ -1,85 +1,32 @@
 package PageObjectClasess;
 
-<<<<<<< HEAD
-=======
 import basePage.BasePage;
->>>>>>> master
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
-<<<<<<< HEAD
-public class HomePage
-{
-    WebDriver driver;
-
-    //constructor
-
-    public HomePage(WebDriver driver)
-    {
-        this.driver = driver;
-        PageFactory.initElements(driver,this);
-    }
-
-    //Locators
-
-    @FindBy(xpath = "//span[text() ='My Account']")
-    private WebElement  myAccountTab;
-
-    @FindBy(xpath ="//a[text()='Register']")
-    private WebElement  registerLink;
-
-
-
-public void ClickOnRegister()
-{
-    myAccountTab.click();
-
-}
-
-public void ClickOnRegisterLink()
-{
-    registerLink.click();
-}
-=======
 public class HomePage extends BasePage
 {
     WebDriver driver;
 
+    public HomePage(WebDriver driver)
+    {
+        super(driver);
+    }
 
-        public HomePage(WebDriver driver)
-        {
-            super(driver);
+    @FindBy(xpath = "//span[text()='My Account']")
+    private WebElement myAccountTab;
 
-        }
+    @FindBy(xpath = "//a[text()='Register']")
+    private WebElement registerLink;
 
+    public void ClickOnRegister()
+    {
+        myAccountTab.click();
+    }
 
-        @FindBy(xpath = "//span[text() ='My Account']")
-        private WebElement myAccountTab;
-
-        @FindBy(xpath = "//a[text()='Register']")
-        private WebElement registerLink;
-
-
-        public void ClickOnRegister ()
-        {
-            myAccountTab.click();
-
-        }
-
-        public void ClickOnRegisterLink ()
-        {
-            registerLink.click();
-        }
-
->>>>>>> master
-
-
+    public void ClickOnRegisterLink()
+    {
+        registerLink.click();
+    }
 }
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> master
--- src/test/java/PageObjectClasess/RegisterPage.java
+++ src/test/java/PageObjectClasess/RegisterPage.java
@@ -1,120 +1,72 @@
 package PageObjectClasess;
 
 import basePage.BasePage;
-<<<<<<< HEAD
-=======
-import org.openqa.selenium.Keys;
->>>>>>> master
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class RegisterPage extends BasePage
 {
     WebDriver driver;
-public RegisterPage(WebDriver driver)
-{
-    super(driver);
-<<<<<<< HEAD
-=======
 
->>>>>>> master
+    public RegisterPage(WebDriver driver)
+    {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//input[@name='firstname']")
+    private WebElement firstNameInput;
+
+    @FindBy(xpath = "//input[@name='lastname']")
+    private WebElement lastNameInput;
+
+    @FindBy(xpath = "//input[@name='email']")
+    private WebElement emailInput;
+
+    @FindBy(xpath = "//input[@name='telephone']")
+    private WebElement telephoneInput;
+
+    @FindBy(xpath = "//input[@name='password']")
+    private WebElement passwordInput;
+
+    @FindBy(xpath = "//input[@name='confirm']")
+    private WebElement confirmInput;
+
+    @FindBy(xpath = "//input[@value='Continue']")
+    private WebElement continueButtonInput;
+
+    public void setFirstName(String firstName)
+    {
+        firstNameInput.sendKeys(firstName);
+    }
+
+    public void setLastName(String lastName)
+    {
+        lastNameInput.sendKeys(lastName);
+    }
+
+    public void setEmail(String email)
+    {
+        emailInput.sendKeys(email);
+    }
+
+    public void setTelephone(String telephone)
+    {
+        telephoneInput.sendKeys(telephone);
+    }
+
+    public void setPassword(String password)
+    {
+        passwordInput.sendKeys(password);
+    }
+
+    public void setConfirm(String confirm)
+    {
+        confirmInput.sendKeys(confirm);
+    }
+
+    public void clickRegisterButton()
+    {
+        continueButtonInput.click();
+    }
 }
-
-//constructor
-
-
-//locators
-@FindBy(xpath = "//input[@name='firstname']")
-<<<<<<< HEAD
-private WebElement firstName;
-
-@FindBy(xpath = "//input[@name='lastname']")
-private WebElement lastName;
-
-@FindBy(xpath = "//input[@name='email']")
-private WebElement email;
-
-@FindBy(xpath = "//input[@name='telephone']")
-private WebElement telephone;
-
-@FindBy(xpath = "//input[@name='password']")
-private WebElement password;
-
-@FindBy(xpath = "//input[@name='confirm']")
-private WebElement confirmPassword;
-
-@FindBy(xpath = "//input[@value='Continue']")
-private WebElement continueButton;
-=======
-private WebElement firstNameInput;
-
-@FindBy(xpath = "//input[@name='lastname']")
-private WebElement lastNameInput;
-
-@FindBy(xpath = "//input[@name='email']")
-private WebElement emailInput;
-
-@FindBy(xpath = "//input[@name='telephone']")
-private WebElement telephoneInput;
-
-@FindBy(xpath = "//input[@name='password']")
-private WebElement passwordInput;
-
-@FindBy(xpath = "//input[@name='confirm']")
-private WebElement confirmInput;
-
-@FindBy(xpath = "//input[@value='Continue']")
-private WebElement continueButtonInput;
-
-//methods;
-
-public void setFirstName(String firstName)
-{
-    firstNameInput.sendKeys(firstName);
-
-
-}
-
-public void setLastName(String lastName)
-{
-    lastNameInput.sendKeys(lastName);
-}
-
-
-public void setEmail(String email)
-{
-    emailInput.sendKeys(email);
-}
-public void setTelephone(String telephone)
-{
-    telephoneInput.sendKeys(telephone);
-
-}
-public void setPassword(String password)
-{
-    passwordInput.sendKeys(password);
-
-}
-public void setConfirm(String confirm)
-{
-    confirmInput.sendKeys(confirm);
-}
-
-public void clickRegisterButton()
-{
-    continueButtonInput.click();
-}
->>>>>>> master
-
-
-
-}
-
-
-
-
-
-
-
